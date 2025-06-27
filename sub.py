3ac0ba71cc51d9cdddd0f3be2bfa03b0eb897c5b
Ali Abdallah <b7ali12345678910@outlook.com>
2025-06-27 06:40:35 +0300

    Update sub.py

--- sub.py
+++ sub.py
@@ -37,7 +37,7 @@ def get_access_token():
 
 # artificially 
 def generate_article(topic: str) -> str:
-    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=AIzaSyBSqog0_0vURPpUzXpP64YXoM2IQakrg3A"
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=AIzaSyAMvSkNNgMgh2oVKTyDDbbpUH_lix4Y_Ws"
     
     headers = {
         "Content-Type": "application/json"
